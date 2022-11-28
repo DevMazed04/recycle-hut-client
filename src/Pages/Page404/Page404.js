8d566a9b1abfe7a290a086d0aa05dfee592fdbe5
DevMazed04 <mdmazedislam2684@gmail.com>
2022-11-28 14:43:37 +0600

    404 not found page added

---
+++ src/Pages/Page404/Page404.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Page404.css';
+
+const Page404 = () => {
+   const bg = "https://i.ibb.co/KWJS1hc/404.jpg"
+   return (
+      <div style=
+         {{ background: `url(${bg})`, backgroundSize: "cover", backgroundRepeat: "repeat" }}>
+         <div className="notfound flex flex-col justify-center items-center text-yellow-400">
+            <div className="notfound-404">
+               <h1 className='text-red-600 font-bold'>404</h1>
+            </div>
+            <h2 className='text-lg'>Oops! This Page Could Not Be Found</h2>
+            <p className='text-sm'>Sorry this page does not exist, have been removed, name changed or temporarily unavailable</p>
+            <div>
+               <Link to="/">
+                  <button className='btn btn-primary mt-4' >Go To Home</button>
+               </Link>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Page404;
